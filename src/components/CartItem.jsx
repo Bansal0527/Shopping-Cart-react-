21dfe814907f2230e035f50c0e9dd344b500bf47
Mayank <116478150+Bansal0527@users.noreply.github.com>
2024-03-04 02:32:44 +0530

    Add files via upload

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,38 @@
+import toast from 'react-hot-toast';
+import {FcDeleteDatabase} from 'react-icons/fc'
+import { useDispatch } from 'react-redux';
+import {remove} from "../redux/Slices/CartSlice"
+
+
+const CartItem = ({item}) => {
+  const dispatch = useDispatch();
+  const removeFromCart = () => {
+      dispatch(remove(item.id));
+      toast.success("Item Removed sucessfully")
+  }
+
+  return (
+    <div>
+
+      <div>
+
+        <div>
+          <img src={item.image} alt="" />
+        </div>
+        <div>
+          <h1>{item.title}</h1>
+          <h1>{item.description}</h1>
+          <div>
+            <p>{item.price}</p>
+            <div 
+            onClick={removeFromCart}>
+              <FcDeleteDatabase/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
